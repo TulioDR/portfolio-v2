9fb3386384dfc230c628999be06b9ae70da0872a
Tulio <tulioruzo29@gmail.com>
2022-08-15 13:31:14 -0400

    fixed section info bug when it was to large

--- src/components/Sections/Info.tsx
+++ src/components/Sections/Info.tsx
@@ -2,9 +2,10 @@ import { motion } from "framer-motion";
 
 type Props = {
    children: React.ReactNode;
+   about?: boolean;
 };
 
-export default function Info({ children }: Props) {
+export default function Info({ children, about }: Props) {
    const sectionInfo = {
       initial: { x: "-100%" },
       animate: {
@@ -15,7 +16,9 @@ export default function Info({ children }: Props) {
    return (
       <motion.div
          variants={sectionInfo}
-         className="text-sm sm:text-base md:text-lg lg:text-3xl font-light"
+         className={`text-sm sm:text-base md:text-lg lg:text-3xl font-light ${
+            about ? "" : "min-w-max"
+         }`}
       >
          {children}
       </motion.div>
--- src/containers/Sections/About.tsx
+++ src/containers/Sections/About.tsx
@@ -24,20 +24,20 @@ export default function About({}: Props) {
          <InfoContainer>
             <SectionTitle>{title}</SectionTitle>
             <Underline />
-            <Info>
+            <Info about>
                <AboutInfoContainer>
                   I'm a self-taught developer that fell in love with React and
                   Next.js.
                </AboutInfoContainer>
             </Info>
-            <Info>
+            <Info about>
                <AboutInfoContainer>
                   I'm a well-organized person, problem solver with high
                   attention to detail and I speak spanish and english. This is
                   more than “just a job” for me.
                </AboutInfoContainer>
             </Info>
-            <Info>
+            <Info about>
                <AboutInfoContainer>
                   It's a passion that motivates me to learn and improve my
                   skills day by day, either with my team or on my own.
