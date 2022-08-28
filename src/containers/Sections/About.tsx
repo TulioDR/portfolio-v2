1a360c6da7d76b5ed58783b089d4fcb2273d1a49
Tulio <tulioruzo29@gmail.com>
2022-08-27 20:09:36 -0400

    about me text changed

--- src/containers/Sections/About.tsx
+++ src/containers/Sections/About.tsx
@@ -18,30 +18,21 @@ type Props = {};
 export default function About({}: Props) {
    const { currentIdiom } = useLanguageContext();
 
-   const { title } = currentIdiom.about;
+   const { title, description1, description2, description3 } =
+      currentIdiom.about;
    return (
       <SectionContainer index={1}>
          <InfoContainer>
             <SectionTitle>{title}</SectionTitle>
             <Underline />
             <Info about>
-               <AboutInfoContainer>
-                  I'm a self-taught developer that fell in love with React and
-                  Next.js.
-               </AboutInfoContainer>
+               <AboutInfoContainer>{description1}</AboutInfoContainer>
             </Info>
             <Info about>
-               <AboutInfoContainer>
-                  I'm a well-organized person, problem solver with high
-                  attention to detail and I speak spanish and english. This is
-                  more than “just a job” for me.
-               </AboutInfoContainer>
+               <AboutInfoContainer>{description2}</AboutInfoContainer>
             </Info>
             <Info about>
-               <AboutInfoContainer>
-                  It's a passion that motivates me to learn and improve my
-                  skills day by day, either with my team or on my own.
-               </AboutInfoContainer>
+               <AboutInfoContainer>{description3}</AboutInfoContainer>
             </Info>
          </InfoContainer>
          <JumbotronContainer>
