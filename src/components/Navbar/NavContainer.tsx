1f19586e71f14376c97f37716b31d07ec6cfa9b6
Tulio <tulioruzo29@gmail.com>
2022-08-31 18:19:15 -0400

    navbar moved down a bit

--- src/components/Navbar/NavContainer.tsx
+++ src/components/Navbar/NavContainer.tsx
@@ -6,8 +6,8 @@ type Props = {
 
 export default function NavContainer({ children }: Props) {
    return (
-      <nav className="section font-light h-24 px-5 md:px-16 lg:px-20 xl:px-10 fixed w-full text-white z-30 md:text-lg lg:text-2xl">
-         <div className="flex justify-between items-center h-full">
+      <nav className="section font-light flex items-end h-24 px-5 md:px-16 lg:px-20 xl:px-10 fixed w-full text-white z-30 md:text-lg lg:text-2xl">
+         <div className="flex justify-between items-center w-full">
             {children}
          </div>
       </nav>
