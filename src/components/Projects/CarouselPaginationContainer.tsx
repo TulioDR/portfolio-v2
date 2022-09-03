3f8d1c3869c3252cfd328a3b57b2bf528116699f
Tulio <tulioruzo29@gmail.com>
2022-09-03 16:35:18 -0400

    project showcase bug fix

--- src/components/Projects/CarouselPaginationContainer.tsx
+++ src/components/Projects/CarouselPaginationContainer.tsx
@@ -17,7 +17,7 @@ export default function CarouselPaginationContainer({
             y: showCarousel ? "0" : "100%",
          }}
          transition={{ duration: 0.2, delay: showCarousel ? 0.5 : 0 }}
-         className={`flex justify-center items-center space-x-3 absolute bottom-5 w-full ${
+         className={`flex justify-center items-center space-x-3 absolute bottom-4 2xl:bottom-20 w-full ${
             showCarousel ? "" : "pointer-events-none"
          }`}
       >
