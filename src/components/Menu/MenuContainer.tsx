6930a7424bd0759647f0d8cdc7a95b780706a483
Tulio <tulioruzo29@gmail.com>
2022-09-03 11:59:22 -0400

    change bg-gradient direction of menu container from right to top

--- src/components/Menu/MenuContainer.tsx
+++ src/components/Menu/MenuContainer.tsx
@@ -12,7 +12,7 @@ export default function MenuContainer({ children }: Props) {
          animate={{ height: "100vh" }}
          exit={{ height: "0", top: 0 }}
          transition={{ duration: 0.5 }}
-         className="fixed left-0 z-20 text-white w-full bg-gradient-to-r from-orange-800 to-orange-900 overflow-hidden"
+         className="fixed left-0 z-20 text-white w-full bg-gradient-to-t from-orange-800 to-orange-900 overflow-hidden"
       >
          <motion.div
             exit={{ opacity: 0 }}
