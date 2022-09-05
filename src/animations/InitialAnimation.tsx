9a299f4891e151f0f541ecf89898743fc2f3729a
Tulio <tulioruzo29@gmail.com>
2022-09-04 22:40:06 -0400

    initial animation bug fix

--- src/animations/InitialAnimation.tsx
+++ src/animations/InitialAnimation.tsx
@@ -7,9 +7,8 @@ type Props = {};
 export default function InitialAnimation({}: Props) {
    return (
       <motion.div
-         className="fixed top-0 left-0 bg-gradient-to-t from-orange-800 to-orange-900 w-full h-screen z-50 grid place-content-center"
+         className="fixed top-0 left-0 bg-gradient-to-t from-orange-800 to-orange-900 w-full h-screen z-50 flex items-center justify-center"
          initial={{ y: 0 }}
-         // animate={{ y: 0 }}
          animate={{ y: "-100%" }}
          transition={{
             duration: 0.8,
@@ -17,13 +16,8 @@ export default function InitialAnimation({}: Props) {
             ease: [0.645, 0.045, 0.355, 1],
          }}
       >
-         <div>
-            <div className="relative h-60">
-               <Image src={eagle} alt="holy eagle" layout="fill" priority />
-            </div>
-            <div className="text-primary text-3xl sm:text-4xl uppercase">
-               Sic Parvis Magna
-            </div>
+         <div className="relative h-60 w-full">
+            <Image src={eagle} alt="holy eagle" layout="fill" priority />
          </div>
       </motion.div>
    );
